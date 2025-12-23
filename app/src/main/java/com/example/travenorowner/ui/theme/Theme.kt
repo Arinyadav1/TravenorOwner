568500a6f2adf1c537534d02a2432b93ccbf73da
Arin Yadav <arinyadav98@gmail.com>
2025-12-23 23:11:03 +0530

    system bar icon bug fix in dark mode

--- app/src/main/java/com/example/travenorowner/ui/theme/Theme.kt
+++ app/src/main/java/com/example/travenorowner/ui/theme/Theme.kt
@@ -9,8 +9,11 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -50,6 +53,16 @@ fun TravenorOwnerTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    val view = LocalView.current
+
+    SideEffect {
+        val window = (view.context as Activity).window
+        WindowCompat.getInsetsController(window, window.decorView).apply {
+            isAppearanceLightStatusBars = true
+            isAppearanceLightNavigationBars = true
+        }
     }
 
     MaterialTheme(
